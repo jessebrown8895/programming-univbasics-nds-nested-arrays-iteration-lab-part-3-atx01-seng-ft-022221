9814889d081e0fd0e068abe0b66107097d7cb757
Jesse Brown <brownjesse8895@yahoo.com>
2021-02-11 01:37:56 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -6,7 +6,7 @@ def join_nested_strings(src)
   element_index = 0 
   while element_index < src[row_index].count do 
    if src[row_index][element_index].is_a?(String) == true
-     src[row_index][element_index]
+     all_strings << src[row_index][element_index]
   end 
     element_index += 1 
 end 
